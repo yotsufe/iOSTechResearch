daa095d0e138a82f1a02d5e7f85b2f45a0c275b6
yotsufe <yotsufe@gmail.com>
2022-01-15 17:10:39 +0900

    ボタンにアクションを追加

--- iOSTechResaerch/iOSTechResaerch/ViewController.swift
+++ iOSTechResaerch/iOSTechResaerch/ViewController.swift
@@ -2,11 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func xibMenuButton(_ sender: UIButton) {
+        openXibMenu()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func openXibMenu() {
+        print("openXibMenu")
+    }
 }
-
