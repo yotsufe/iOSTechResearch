1c351cc6ca3c4244a8af6287fd12b2873ddbc0cf
yotsufe <yotsufe@gmail.com>
2022-01-25 01:06:22 +0900

    Navigation Bar設置

--- iOSTechResaerch/iOSTechResaerch/ViewController/MainViewController.swift
+++ iOSTechResaerch/iOSTechResaerch/ViewController/MainViewController.swift
@@ -8,7 +8,7 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Main"
     }
 
     func openXibMenu() {
