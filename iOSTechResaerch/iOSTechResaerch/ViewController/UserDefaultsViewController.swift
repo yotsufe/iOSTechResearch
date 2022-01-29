e438f7ecd98d743b4736f20e26a679ac2a0d9b44
yotsufe <yotsufe@gmail.com>
2022-01-29 23:41:43 +0900

    Alert表示

--- iOSTechResaerch/iOSTechResaerch/ViewController/UserDefaultsViewController.swift
+++ iOSTechResaerch/iOSTechResaerch/ViewController/UserDefaultsViewController.swift
@@ -4,10 +4,13 @@ class UserDefaultsViewController: UIViewController {
 
     private let constUserDefaultsTextField = "UserDefaultsTextField"
 
+    private var alertController: UIAlertController!
+
     @IBOutlet var userDefaultsTextField: UITextField!
 
     @IBAction func saveUserDefaultsString(_ sender: Any) {
         UserDefaults.standard.set(userDefaultsTextField.text, forKey: constUserDefaultsTextField)
+        alert(title: "Save", message: userDefaultsTextField.text ?? "")
     }
 
     override func viewDidLoad() {
@@ -19,4 +22,11 @@ class UserDefaultsViewController: UIViewController {
     private func setUserDefaultsTextField() {
         userDefaultsTextField.text = UserDefaults.standard.string(forKey: constUserDefaultsTextField)
     }
+
+    private func alert(title:String, message:String) {
+        alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Success", style: .default, handler: nil))
+        present(alertController, animated: true)
+    }
+
 }
