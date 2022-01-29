ae0c886994414fc469ea251bc97f36de6e2890f7
yotsufe <yotsufe@gmail.com>
2022-01-29 23:12:23 +0900

    UserDefaultsの保存取得

--- iOSTechResaerch/iOSTechResaerch/ViewController/UserDefaultsViewController.swift
+++ iOSTechResaerch/iOSTechResaerch/ViewController/UserDefaultsViewController.swift
@@ -2,8 +2,21 @@ import UIKit
 
 class UserDefaultsViewController: UIViewController {
 
+    private let constUserDefaultsTextField = "UserDefaultsTextField"
+
+    @IBOutlet var userDefaultsTextField: UITextField!
+
+    @IBAction func saveUserDefaultsString(_ sender: Any) {
+        UserDefaults.standard.set(userDefaultsTextField.text, forKey: constUserDefaultsTextField)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "UserDefaults"
+        setUserDefaultsTextField()
+    }
+
+    private func setUserDefaultsTextField() {
+        userDefaultsTextField.text = UserDefaults.standard.string(forKey: constUserDefaultsTextField)
     }
 }
