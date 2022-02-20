a62c4811e8ca4539e6c382462600059149150f64
yotsufe <yotsufe@gmail.com>
2022-02-20 23:38:33 +0900

    TableView表示

--- iOSTechResaerch/iOSTechResaerch/ViewController/Http/QiitaListViewController.swift
+++ iOSTechResaerch/iOSTechResaerch/ViewController/Http/QiitaListViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
 
-class QiitaListViewController: UIViewController {
+class QiitaListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "QiitaList"
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    let items = ["one", "two", "three", "four"]
 }
