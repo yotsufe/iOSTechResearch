3e79ef347ba41847c704c02a113f78cb44e8e2db
yotsufe <yotsufe@gmail.com>
2022-05-31 21:33:45 +0900

    記事JSON取得

--- iOSTechResaerch/iOSTechResaerch/ViewController/Http/QiitaListViewController.swift
+++ iOSTechResaerch/iOSTechResaerch/ViewController/Http/QiitaListViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class QiitaListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -7,14 +9,18 @@ class QiitaListViewController: UIViewController, UITableViewDelegate, UITableVie
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "QiitaList"
+
+        view.addSubview(tableView)
         self.tableView.dataSource = self
         self.tableView.delegate = self
+
+        getQiitaArticles()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.textLabel?.text = items[indexPath.row]
@@ -22,4 +28,19 @@ class QiitaListViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     let items = ["one", "two", "three", "four"]
+
+    func getQiitaArticles() {
+        AF.request("https://qiita.com/api/v2/items", method: .get, parameters: nil, encoding: JSONEncoding.default)
+            .responseJSON { [self] response in
+                let data = JSON(response.value as Any)
+                print("data: \(data)")
+                data.forEach { (_, data) in
+                    let article: [String: String?] = [
+                        "title": data["title"].string,
+                        "userId": data["user"]["id"].string
+                    ]
+                    print("###article: \(article)")
+                }
+            }
+    }
 }
